c8a7bc3bdae3d453c2c343efce65ce10e262ea14
steelover <150594943+steelover@users.noreply.github.com>
2024-03-10 22:30:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,64 +2,49 @@
 #include <cstdint>
 #include "alg.h"
 
-double pown(double value, uint16_t n)
-{
-	double exponent = 1;
-	for (uint16_t i = 0; i < n; i++)
-	{
-		exponent *= value;
-	}
-	return exponent;
+double pown(double value, uint16_t n) {
+    double exponent = 1;
+    for (uint16_t i = 0; i < n; i++) {
+        exponent *= value;
+    }
+    return exponent;
 }
 
-uint64_t fact(uint16_t n)
-{
-	uint64_t factor = 1;
-	if (n == 0 || n == 1)
-	{
-		return factor;
-	}
-	else
-	{
-		for (uint16_t j = 1; j < n+1; j++)
-		{
-			factor *= j;
-		}
-		return factor;
-	}
+uint64_t fact(uint16_t n) {
+    uint64_t factor = 1;
+    if (n == 0 || n == 1) {
+        return factor;
+    } else {
+        for (uint16_t j = 1; j < n + 1; j++) {
+            factor *= j;
+        }
+        return factor;
+    }
 }
 
-double calcitem(double x, uint16_t n)
-{
-	return (pown(x, n) / fact(n));
+double calcitem(double x, uint16_t n) { return (pown(x, n) / fact(n)); }
+
+double expn(double x, uint16_t count) {
+    double summ = 0;
+    for (uint16_t a = 0; a < count + 1; a++) {
+        summ += calcitem(x, a);
+    }
+    return summ;
 }
 
-double expn(double x, uint16_t count)
-{
-	double summ = 0;
-	for (uint16_t a = 0; a < count + 1; a++)
-	{
-		summ += calcitem(x, a);
-	}
-	return summ;
+double sinn(double x, uint16_t count) {
+    double summ = 0;
+    for (uint16_t a = 1; a < count - 1; a++) {
+        summ += pown(-1, a - 1) * calcitem(x, 2 * a - 1);
+    }
+    return summ;
 }
 
-double sinn(double x, uint16_t count)
-{
-	double summ = 0;
-	for (uint16_t a = 1; a < count - 1; a++)
-	{
-		summ += pown(-1, a - 1) * calcitem(x, 2 * a - 1);
-	}
-	return summ;
+double cosn(double x, uint16_t count) {
+    double summ = 0;
+    for (uint16_t a = 1; a < count - 1; a++) {
+        summ += pown(-1, a - 1) * calcitem(x, 2 * a - 2);
+    }
+    return summ;
 }
 
-double cosn(double x, uint16_t count)
-{
-	double summ = 0;
-	for (uint16_t a = 1; a < count - 1; a++)
-	{
-		summ += pown(-1, a - 1) * calcitem(x, 2 * a - 2);
-	}
-	return summ;
-}
